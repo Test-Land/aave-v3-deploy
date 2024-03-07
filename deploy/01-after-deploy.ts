73d859d7c38f1f5ab42b7c7aeb79dd0631067a46
0xchamel <mauroliu77@gmail.com>
2024-03-07 19:43:25 +1000

    feat: sepolia configuration for USDC, USDT, WETH & DAI

--- deploy/01-after-deploy.ts
+++ deploy/01-after-deploy.ts
@@ -1,7 +1,4 @@
-import {
-  isTestnetMarket,
-  loadPoolConfig,
-} from "./../helpers/market-config-helpers";
+import { isTestnetMarket, loadPoolConfig } from "./../helpers/market-config-helpers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { MARKET_NAME } from "../helpers/env";
@@ -11,50 +8,46 @@ import { getPoolConfiguratorProxy, waitForTx } from "../helpers";
  * The following script runs after the deployment starts
  */
 
-const func: DeployFunction = async function ({
-  getNamedAccounts,
-  deployments,
-  ...hre
-}: HardhatRuntimeEnvironment) {
-  console.log("=== Post deployment hook ===");
-  const poolConfig = loadPoolConfig(MARKET_NAME);
+const func: DeployFunction = async function ({ getNamedAccounts, deployments, ...hre }: HardhatRuntimeEnvironment) {
+    console.log("=== Post deployment hook ===");
+    const poolConfig = loadPoolConfig(MARKET_NAME);
 
-  console.log("- Enable stable borrow in selected assets");
-  await hre.run("review-stable-borrow", { fix: true, vvv: true });
+    console.log("- Enable stable borrow in selected assets");
+    await hre.run("review-stable-borrow", { fix: true, vvv: true });
 
-  console.log("- Review rate strategies");
-  await hre.run("review-rate-strategies");
+    console.log("- Review rate strategies");
+    await hre.run("review-rate-strategies");
 
-  console.log("- Setup Debt Ceiling");
-  await hre.run("setup-debt-ceiling");
+    console.log("- Setup Debt Ceiling");
+    await hre.run("setup-debt-ceiling");
 
-  console.log("- Setup Borrowable assets in Isolation Mode");
-  await hre.run("setup-isolation-mode");
+    console.log("- Setup Borrowable assets in Isolation Mode");
+    await hre.run("setup-isolation-mode");
 
-  console.log("- Setup E-Modes");
-  await hre.run("setup-e-modes");
+    console.log("- Setup E-Modes");
+    await hre.run("setup-e-modes");
 
-  console.log("- Setup Liquidation protocol fee");
-  await hre.run("setup-liquidation-protocol-fee");
+    console.log("- Setup Liquidation protocol fee");
+    await hre.run("setup-liquidation-protocol-fee");
 
-  if (isTestnetMarket(poolConfig)) {
-    // Disable faucet minting and borrowing of wrapped native token
-    await hre.run("disable-faucet-native-testnets");
-    console.log("- Minting and borrowing of wrapped native token disabled");
+    // if (isTestnetMarket(poolConfig)) {
+    //   // Disable faucet minting and borrowing of wrapped native token
+    //   await hre.run("disable-faucet-native-testnets");
+    //   console.log("- Minting and borrowing of wrapped native token disabled");
 
-    // Unpause pool
-    const poolConfigurator = await getPoolConfiguratorProxy();
-    await waitForTx(await poolConfigurator.setPoolPause(false));
-    console.log("- Pool unpaused and accepting deposits.");
-  }
+    //   // Unpause pool
+    //   const poolConfigurator = await getPoolConfiguratorProxy();
+    //   await waitForTx(await poolConfigurator.setPoolPause(false));
+    //   console.log("- Pool unpaused and accepting deposits.");
+    // }
 
-  if (process.env.TRANSFER_OWNERSHIP === "true") {
-    await hre.run("transfer-protocol-ownership");
-    await hre.run("renounce-pool-admin");
-    await hre.run("view-protocol-roles");
-  }
+    if (process.env.TRANSFER_OWNERSHIP === "true") {
+        await hre.run("transfer-protocol-ownership");
+        await hre.run("renounce-pool-admin");
+        await hre.run("view-protocol-roles");
+    }
 
-  await hre.run("print-deployments");
+    await hre.run("print-deployments");
 };
 
 func.tags = ["after-deploy"];
--- markets/test/index.ts
+++ markets/test/index.ts
@@ -1,69 +1,64 @@
-import {
-  rateStrategyStableOne,
-  rateStrategyStableTwo,
-  rateStrategyVolatileOne,
-} from "./rateStrategies";
+import { rateStrategyStableOne, rateStrategyStableTwo, rateStrategyVolatileOne } from "./rateStrategies";
 import { ZERO_ADDRESS } from "../../helpers";
-import {
-  IAaveConfiguration,
-  eEthereumNetwork,
-  eArbitrumNetwork,
-} from "../../helpers/types";
+import { IAaveConfiguration, eEthereumNetwork, eArbitrumNetwork } from "../../helpers/types";
 
 import { CommonsConfig } from "./commons";
-import {
-  strategyDAI,
-  strategyUSDC,
-  strategyAAVE,
-  strategyLINK,
-  strategyWETH,
-} from "./reservesConfigs";
+import { strategyDAI, strategyUSDC, strategyAAVE, strategyLINK, strategyWETH, strategyUSDT } from "./reservesConfigs";
 
 // ----------------
 // POOL--SPECIFIC PARAMS
 // ----------------
 
 export const AaveMarket: IAaveConfiguration = {
-  ...CommonsConfig,
-  MarketId: "Testnet Aave Market",
-  ProviderId: 8080,
-  ReservesConfig: {
-    AAVE: strategyAAVE,
-    DAI: strategyDAI,
-    USDC: strategyUSDC,
-    WETH: strategyWETH,
-    LINK: strategyLINK,
-  },
-  ReserveAssets: {
-    [eEthereumNetwork.main]: {
-      AAVE: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
-      DAI: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-      USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-      WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-      LINK: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+    ...CommonsConfig,
+    MarketId: "Testnet Aave Market",
+    ProviderId: 8080,
+    ReservesConfig: {
+        DAI: strategyDAI,
+        USDT: strategyUSDT,
+        // AAVE: strategyAAVE,
+        USDC: strategyUSDC,
+        WETH: strategyWETH,
+        // LINK: strategyLINK,
     },
-    [eEthereumNetwork.kovan]: {
-      AAVE: ZERO_ADDRESS,
-      DAI: ZERO_ADDRESS,
-      USDC: ZERO_ADDRESS,
-      WETH: ZERO_ADDRESS,
-      LINK: ZERO_ADDRESS,
+    ReserveAssets: {
+        [eEthereumNetwork.main]: {
+            // AAVE: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+            // DAI: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+            // USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+            // WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+            // LINK: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+            // DAI: "0xFe0a2a42174d626FeF4b8C1dD2BA51BbD82cB546",
+            // USDT: "0x9FBb8863005a30c9888C5EBe096076e8062Cd221",
+        },
+        [eEthereumNetwork.kovan]: {
+            AAVE: ZERO_ADDRESS,
+            DAI: ZERO_ADDRESS,
+            USDC: ZERO_ADDRESS,
+            WETH: ZERO_ADDRESS,
+            LINK: ZERO_ADDRESS,
+        },
+        [eArbitrumNetwork.arbitrumTestnet]: {
+            AAVE: ZERO_ADDRESS,
+            DAI: ZERO_ADDRESS,
+            USDC: ZERO_ADDRESS,
+            WETH: ZERO_ADDRESS,
+            LINK: ZERO_ADDRESS,
+        },
+        [eEthereumNetwork.rinkeby]: {
+            AAVE: ZERO_ADDRESS,
+            DAI: ZERO_ADDRESS,
+            USDC: ZERO_ADDRESS,
+            WETH: ZERO_ADDRESS,
+            LINK: ZERO_ADDRESS,
+        },
+        [eEthereumNetwork.sepolia]: {
+            DAI: "0xFe0a2a42174d626FeF4b8C1dD2BA51BbD82cB546",
+            USDT: "0x9FBb8863005a30c9888C5EBe096076e8062Cd221",
+            USDC: "0xA0029c1f90a1A449b437A8C0DBEf2e5f3D80108c",
+            WETH: "0x7b79995e5f793a07bc00c21412e50ecae098e7f9",
+        },
     },
-    [eArbitrumNetwork.arbitrumTestnet]: {
-      AAVE: ZERO_ADDRESS,
-      DAI: ZERO_ADDRESS,
-      USDC: ZERO_ADDRESS,
-      WETH: ZERO_ADDRESS,
-      LINK: ZERO_ADDRESS,
-    },
-    [eEthereumNetwork.rinkeby]: {
-      AAVE: ZERO_ADDRESS,
-      DAI: ZERO_ADDRESS,
-      USDC: ZERO_ADDRESS,
-      WETH: ZERO_ADDRESS,
-      LINK: ZERO_ADDRESS,
-    },
-  },
 };
 
 export default AaveMarket;
